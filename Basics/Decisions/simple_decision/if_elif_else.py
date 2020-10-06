b7af893d670bfb59998f6b5a761dc993247228ce
repl.it user <replituser@example.com>
2020-10-06 10:15:05 +0000

    Added code for comparing nunbers

---
+++ Basics/Decisions/simple_decision/if_elif_else.py
@@ -0,0 +1,15 @@
+print ("Towards which direction should I paint (up, down, left or right?")
+
+direction = input()
+
+if direction == "up":
+  print ("I am painting in the upward direction")
+elif direction == "down":
+  print ("I am painting in the downward direction")
+elif direction == "left":
+  print ("I am painting to the left")
+elif direction == "right":
+  print ("I am painting to the right")
+else: 
+  print("I don't know what I'm doing!!")
+
