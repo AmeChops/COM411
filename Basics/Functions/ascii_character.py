efec634fd1472d4d276bc9d1f6d307ddb0c990e4
repl.it user <replituser@example.com>
2020-10-13 11:12:06 +0000

    updated code

--- Basics/Functions/ascii_character.py
+++ Basics/Functions/ascii_character.py
@@ -1,15 +1,13 @@
 print("Program started!")
 print("Please enter an ASCII code")
 
-acode = int(input())
+acode = abs(int(input()))
 
-bottom = 32
-top = 127
-
-if acode in range(bottom, top):
+if (acode >= 32 and acode <= 126):
   letter = chr(acode)
   print("The character represented by the ASCII code {} is {}".format(acode, letter))
 
 else:
-  print("The calue entered is outside of the ASCII code range")
-  print("Program ended!")
+  print("The value entered is outside of the ASCII code range")
+
+print("Program ended!")
--- Basics/Functions/ascii_code.py
+++ Basics/Functions/ascii_code.py
@@ -1,8 +1,11 @@
 print("Program started!")
-
 print("Please enter a standard character")
 
 letter = input()
+
+if (len(letter) == 1):
+  ascii_value = ord(letter)
+  print("The ASCII code for {} is {}.".format(letter), ascii_value)
 
 if len(letter) > 1:
   print("enter 1 character only")
