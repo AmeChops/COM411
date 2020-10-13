ea02877f7bed73454d7f544d3a47ef4d7c66d288
repl.it user <replituser@example.com>
2020-10-13 09:48:58 +0000

    updated code for ascii return

--- Basics/Functions/ascii_code.py
+++ Basics/Functions/ascii_code.py
@@ -4,11 +4,11 @@ print("Please enter a standard character")
 
 letter = input()
 
-if letter == len(1):
-  print("The letter", letter, "as ASCII code is {}")
-elif letter > len(1):
-  print ("enter 1 character only")
-else:
+
+if len(letter) > 1:
   print("enter 1 character only")
+
+else:
+  print("The letter", letter, "as ASCII code is {}".format(ord(letter)))
 
 print("Program ended!")
