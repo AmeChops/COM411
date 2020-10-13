d919f6aa4ab30772d7f033f733643e281058b7b3
repl.it user <replituser@example.com>
2020-10-13 09:12:49 +0000

    updated ascii code file

---
+++ Basics/Decisions/simple_decision/counter.py
@@ -0,0 +1,11 @@
+print("please enter the first whole number")
+
+first_number = (input(int))
+
+print("Please enter the second whole number")
+
+second_number = (input(int))
+
+print("Please enter the third whole number")
+
+third_number = (input(int))
---
+++ Basics/Functions/ascii_code.py
@@ -0,0 +1,14 @@
+print("Program started!")
+
+print("Please enter a standard character")
+
+letter = input()
+
+if letter == len(1):
+  print("The letter", letter, "as ASCII code is {}")
+elif letter > len(1):
+  print ("enter 1 character only")
+else:
+  print("enter 1 character only")
+
+print("Program ended!")
