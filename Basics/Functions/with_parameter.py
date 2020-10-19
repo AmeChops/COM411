41066e2ff88f1639f353b989fddd10d4278a90ed
repl.it user <replituser@example.com>
2020-10-19 11:10:05 +0000

    updated functions code

--- Basics/Functions/with_parameter.py
+++ Basics/Functions/with_parameter.py
@@ -1,14 +1,17 @@
+# The function with a single parameter named 'plan'
 def escape_by(plan):
-  if (plan == "jump over"):
-    print ("we cannot escape by jumping over as the boulder is too big")
-  elif (plan == "run around"):
-    print ("we cannot escape by running around as the boulder is too fast")
-  elif (plan == "go deeper"):
-    print ("that may work let's go deeper")
-  else:
-    print("Wecannot escape that way as the boulder is in the way")
+    # Determine which message to display
+    if (plan == "jumping over"):
+        print("We cannot escape that way! The boulder is too big!")
+    elif (plan == "running around"):
+        print("We cannot escape that way! The boulder is moving too fast!")
+    elif (plan == "going deeper"):
+        print("That might just work! Let's go deeper!")
+    else:
+        print("Not sure about that plan!")
 
-escape_by("jump over")
-escape_by("run around")
-escape_by("go deeper")
+# Calls to the function
+escape_by("jumping over")
+escape_by("running around")
+escape_by("going deeper") 
 
