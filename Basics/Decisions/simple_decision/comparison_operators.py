66ffacd28d0e84cf1e97f04282a509f43e8c7624
repl.it user <replituser@example.com>
2020-10-19 09:58:48 +0000

    updated comparison_operator

--- Basics/Decisions/simple_decision/comparison_operators.py
+++ Basics/Decisions/simple_decision/comparison_operators.py
@@ -1,14 +1,14 @@
+# Ask user for the direction 
 print("Please enter the first number.")
+first_number = int(input()) 
 
-first_number = input()
+print("Please enter the second number.")
+second_number = int(input())
 
-print ("Please enter the second number.")
-
-second_number = input()
-
-if first_number > second_number:
-  print("second number is the smallest")
-elif first_number < second_number:
-  print ("first number is the biggest")
+# Determine which message to display
+if (first_number < second_number):
+    print("\nThe first number is the smallest.")
+elif (first_number > second_number):
+    print("\nThe second number is the smallest.")
 else:
-  print("numbers are equal")
+    print("\nThe two numbers are equal.")
