e0bc25f78641d6c9907263573ab62b06dd290f46
repl.it user <replituser@example.com>
2020-10-19 09:42:38 +0000

    update uder input

--- Basics/Input/user_input.py
+++ Basics/Input/user_input.py
@@ -1,4 +1,8 @@
 # Ask user to enter their name
 print("What is your name human?")
+
+# Read user's repsonse
 name = input()
+
+# Display confrimation message
 print("It is nice to meet you human", name)
