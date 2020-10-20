b10fe4ab02984c5d0e69fa739b8e499883a4eef7
repl.it user <replituser@example.com>
2020-10-20 09:08:08 +0000

    Code added for simple list

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
-def sum_weights(beep_weight, bop_weight):
-    total_weight = beep_weight + bop_weight
-    return total_weight
+# setting up the function with no parameters 
+def myth():
+  # displaying initial message
+  print ("Who wishes to learn about the demidog Maui?")  
 
-def calc_avg_weight(beep_weight, bop_weight):
-    avg_weight = (beep_weight + bop_weight) / 2
-    return avg_weight
+  # reading and storing user's response to question above
+  user_name = input()
 
-def run():
-    # retrieve required user data
-    print("What is the weight of Bop?")
-    bop_weight = float(input())
+  # displaying the story message inserting the users input from previous step
+  print("Well, {}, he stole the heart of the goddess Te Fiti.".format(user_name))
 
-    print("What is the weight of Beep?")
-    beep_weight = float(input())
+  # displaying the final message here (requirement 5 above)
+  print("You must find Maui and lift the curse!")
 
-    print("What would you like to calculate (sum or average)?")
-    action = input()
-
-    # determine and carry out action
-    if (action == "sum"):
-        answer = sum_weights(beep_weight, bop_weight)
-        print("The sum of Beep's and Bop's weight is {:.2f}".format(answer))
-    elif (action == "average"):
-        answer = calc_avg_weight(beep_weight, bop_weight)
-        print("The average of Beep's and Bop's weight is {:.2f}".format(answer))
-    else:
-        print("I am not sure what you would like to do.")
+# calling the function with no specified parameter as it is being read from the users input above
+myth()
