78ea11b7c8e05ac59d6686860165834f53d8b1b2
repl.it user <replituser@example.com>
2020-10-20 11:19:19 +0000

    updated index code

--- Data/Lists/index_list.py
+++ Data/Lists/index_list.py
@@ -1,13 +1,18 @@
 def movements():
   path = ["Move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1]
-  return(path)
+  
+  return path
 
 def run():
   print ("Moving...")
   direction = movements()
-  print("{} for {} steps".format(direction[0], direction[1]))
-  print("{} for {} steps".format(direction[2], direction[3]))
-  print("{} for {} steps".format(direction[4], direction[5]))
-  print("{} for {} steps".format(direction[6], direction[7]))
+  
+  for index in range(0, len(direction), 2):
+    print("{} for {} steps".format(direction[index], direction[index+1]))
+  
+  # print("{} for {} steps".format(direction[0], direction[1]))
+  # print("{} for {} steps".format(direction[2], direction[3]))
+  # print("{} for {} steps".format(direction[4], direction[5]))
+  # print("{} for {} steps".format(direction[6], direction[7]))
 
 run()
