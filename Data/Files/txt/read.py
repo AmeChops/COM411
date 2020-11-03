b97af23a075515f182d3d2937da1035a8c45c812
repl.it user <replituser@example.com>
2020-11-03 10:59:22 +0000

    added code for reading locations

--- Data/Files/txt/read.py
+++ Data/Files/txt/read.py
@@ -1,2 +1,14 @@
-import os
-  path = os.getcwd()
+def search(filename):
+  print("Searching...")
+  
+  with open(filename) as file:
+    for line in file:
+      print(f"Looked in the {line}\n")
+  
+  print("Done!")
+
+def run():
+  location = search("Data/Files/txt/locations.txt")
+  return location
+  
+run()
