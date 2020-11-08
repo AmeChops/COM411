4175961b46e5c5d79a9767809b1d9508a5c9689c
repl.it user <replituser@example.com>
2020-11-08 15:35:49 +0000

    updated all codes

---
+++ Assessments/Ass_2_1.py
@@ -0,0 +1,21 @@
+# setting up the function with no parameters
+def gang():
+  
+  # displaying initial message
+  print("Loading gang...")
+  
+  # creating list called 'friends'
+  friends = []
+  
+  # populating list called 'friends'
+  friends.append("Scooby Doo")
+  friends.append("Shaggy Rogers")
+  friends.append("Fred Jones")
+  friends.append("Daphne Blake") 
+  friends.append("Velma Dinkley")
+
+  # displaying the contents of the list 'friends' with final message
+  print(friends, "\n...Done!")
+
+# calling the function with no specified parameter
+gang()
--- main.py
+++ main.py
@@ -1,38 +1,5 @@
-def observed():
-  observations = []
-  for count in range(5):
-    print("Please enter a value:")
-    item = input()
-    observations.append(item)
 
-  return observations
 
-def remove_observations(observations):
-  is_running = True
 
-  while(is_running):
-    print("do you wish to remove and observation (yes/no)?")
-    answer = input()
-
-    if (answer == "yes"):
-      print("What would you like to remove?")
-      to_remove = input()
-      observations.remove(to_remove)
-    else:
-      is_running = False
-    
-def run():
-  observations = observed()
-  remove_observations(observations)
-
-  observations_set = set()
-  
-  for observation in observations:
-    occurences = observations.count(observation)
-    observations_set.add((observation, occurences))
-  
-  for key, value in sorted(observations_set):
-    print("{key} observed {value} times.")
-
-run()
+ 
 
