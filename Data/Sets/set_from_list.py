c344a091dfa640eee290af8aaeb90d65c7a25dc4
repl.it user <replituser@example.com>
2020-11-08 13:20:02 +0000

    updated codes for week 5

--- Data/Sets/set_from_list.py
+++ Data/Sets/set_from_list.py
@@ -2,21 +2,23 @@ def observed():
   observations = []
 
   for count in range(7):
-    print("Please enter an observation: ")
-    observation = input
-    observations.append(observation)
-  
+    print("Please enter an observation:")
+    observations.append(input())
+
   return observations
 
 def run():
   print("Counting observations...")
   observations = observed()
-  observations_set = set()
 
+  # populate set
+  observations_set = set()
   for observation in observations:
-    occurences = observations.count(observation)
-    observations_set.add((observation, occurences))
-   
-  print(observations_set)
+    data = (observation, observations.count(observation))
+    observations_set.add(data)
+
+  # display set
+  for data in observations_set:
+    print(f"{data[0]} observed {data[1]} times.")
 
 run()
--- Data/Sets/simple_set.py
+++ Data/Sets/simple_set.py
@@ -1,10 +1,8 @@
 def observed():
-  observations = set(["Flying Car", "Sky Scraper", "Laser", "Dome"])
-  
+  observations = {"Flying Car", "Sky Scraper", "Laser", "Dome"}
   return observations
 
 def run():
-  observed()
   print(observed())
 
 run()
