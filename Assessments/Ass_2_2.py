d0f67b48f85a44e2d6268874f75cec2579e4867a
repl.it user <replituser@example.com>
2020-11-12 14:40:38 +0000

    updated ass 2

--- Assessments/Ass_2_2.py
+++ Assessments/Ass_2_2.py
@@ -18,3 +18,20 @@ run()
 
   
 
+def phrases():
+  quotes = {}
+  friends = ["Shaggy", "Scooby"]
+
+  for friend in friends:  
+    print("What does {} say?".format(friends))
+    quotes[name] = friend
+    quote = input
+
+  return phrases
+  
+def run():
+  phrases()
+  mylists = phrases.quote
+  return mylists
+
+run()
