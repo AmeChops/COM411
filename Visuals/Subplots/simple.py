6a104ad5495f2b70f1794d72d0adbc452f46a3bf
repl.it user <replituser@example.com>
2020-11-17 12:16:06 +0000

    updated simple plots code

--- Visuals/Subplots/simple.py
+++ Visuals/Subplots/simple.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
 
-def read_data(file_path):
-  temps = []
-  file = open(file_path)
-  lines = file.readlines()
-  for line in lines:
-    temps.append(line.strip())
-  file.close()
-  return temps
- 
+def read_data(filepath):
+  with open(filepath) as file:
+    temperatures = []
+    for line in file:
+      temperature = float(line.strip())
+      temperatures.append(temperature)
+
+  return temperatures
+
 def run():
-  data = read_data("Visuals/Subplots/temps.txt")
-  fig, axes = plt.subplots(2,2)
-  axes[0,0].plot(data)
+  data = read_data('Visuals/Subplots/temps.txt')
+  fig, (ax1, ax2) = plt.subplots(1, 2)
+
+  ax1.plot(range(1, 8), data)
+  ax2.bar(range(1, 8), data)
+  plt.tight_layout()
   plt.show()
-  
-run() 
+
+run()
--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-import matplotlib.pyplot as plt
 
-def read_data(file_path):
-  temps = []
-  file = open(file_path)
-  lines = file.readlines()
-  for line in lines:
-    temps.append(line.strip())
-  file.close()
-  return temps
- 
-def run():
-  data = read_data("Visuals/Subplots/temps.txt")
-  fig, axes = plt.subplots(2,2)
-  axes[0,0].plot(data)
-  plt.show()
-  
-run() 
