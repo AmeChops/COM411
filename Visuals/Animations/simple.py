4058dbcf4de54fd6feb9a26d9be5ccfd190913cc
repl.it user <replituser@example.com>
2020-11-24 11:06:45 +0000

    updated the code for the simple chart

--- Visuals/Animations/simple.py
+++ Visuals/Animations/simple.py
@@ -1,17 +1,31 @@
+#importing the relevant modules
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+#setting global variable
 fig, ax = plt.subplots()
 
+#creating function with 1 parameter
 def animate(frame):
+  
+  #setting limits on the x and y axis
   ax.set_xlim(0, 10)
   ax.set_ylim(0, 10)
+  
+  #setting the plots to the current frame number and display a red circle marker 
   ax.plot(frame, frame, 'ro')
 
+#creating function to run the program
 def run():
+  
+  #calling the global variable
   global fig
+
+  #setting the animation type with frames and intervals
   simple_animation = animation.FuncAnimation(fig, animate, frames = 10, interval = 1000)
 
+  #ensuring the chart displays
   plt.show()
 
+#calling the function to run the program
 run()
